ea630fb2477941b673114f502102edbc6f32f66d
Bahadır Yağan <bahadir.yagan@gmail.com>
2014-11-16 19:57:10 +0200

    format and comment

--- src/main/resources/public/js/viewer.js
+++ src/main/resources/public/js/viewer.js
@@ -2,12 +2,7 @@
 	'use strict';
 
 	app = angular.module('viewer', []);
-	app.controller('ViewerController', [
-			'$http',
-			'$location',
-			'$routeParams',
-			'$scope',
-			'marked',
+	app.controller('ViewerController', [ '$http', '$location', '$routeParams', '$scope', 'marked',
 			function($http, $location, $routeParams, $scope, marked) {
 				var viewer = this;
 				this.content = "File content here!";
@@ -15,18 +10,17 @@
 				this.frontMatter = '';
 				this.isMarkdown = this.file.split('.').pop() == 'md';
 
-				$http.get('/api/resource?path=' + viewer.file).success(
-						function(data) {
-							viewer.content = data;
-							if(viewer.isMarkdown) {
-								viewer.content = viewer.content.replace(/---[\s\S]*?---/, '');
-							}
-						});
+				$http.get('/api/resource?path=' + viewer.file).success(function(data) {
+					viewer.content = data;
+					if (viewer.isMarkdown) {
+						// Remove front matter (jekyll)
+						viewer.content = viewer.content.replace(/---[\s\S]*?---/, '');
+					}
+				});
 
 				this.edit = function() {
 					$location.path('/editor/' + viewer.file);
 				};
-
 
 			} ]);
 }());
